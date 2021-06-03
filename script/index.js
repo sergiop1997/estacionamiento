e43496ee0881185c27bbfe8ab2397b5a3633b9ae
Sergio perez <saperezi@ufpso.edu.co>
2021-06-03 01:00:00 -0500

    Agregando una validacion al formulario

--- script/index.js
+++ script/index.js
@@ -10,18 +10,12 @@ firebase.initializeApp({
 
 const db = firebase.firestore();
 
-console.log(db);
-
 //Guardar vehiculos
-const guardarVehiculo = () => {
+const guardarVehiculo = vehiculo => {
   db.collection('vehiculos')
-    .add({
-      nombrePropietario: 'wiliam',
-      tipo: 'carro',
-      placa: 'xxx-ccc',
-      hora: '21:34',
-    })
+    .add(vehiculo)
     .then(docRef => {
+      mostrarGuardadoExitoso();
       console.log('Agregado');
     })
     .catch(error => {
@@ -33,9 +27,6 @@ const guardarVehiculo = () => {
 
 //Consulta de vehiculos
 const consultarVehiculos = () => {
-  // const result = await db.collection('vehiculos').get();
-  // console.log(result);
-
   db.collection('vehiculos').onSnapshot(querySnapshot => {
     querySnapshot.forEach(element => {
       console.log(element.data());
@@ -43,7 +34,7 @@ const consultarVehiculos = () => {
   });
 };
 
-consultarVehiculos();
+// consultarVehiculos();
 
 //Editar Vehiculo
 const editarVehiculo = id => {
@@ -51,10 +42,10 @@ const editarVehiculo = id => {
 
   return washingtonRef
     .update({
-      nombrePropietario,
-      tipo,
-      placa,
-      hora,
+      nombrePropietario: 'Sergio',
+      tipo: 'Cicla',
+      placa: 'CCD-FCSD',
+      hora: '34:43',
     })
     .then(() => {
       console.log('Vehiculo actualizado');
@@ -63,6 +54,8 @@ const editarVehiculo = id => {
       console.error('Error updating document: ', error);
     });
 };
+
+// editarVehiculo('fz2D16LfsTWVRmpmX2Po');
 
 //Eliminar vehiculo
 const eliminarVehiculo = id => {
@@ -77,4 +70,65 @@ const eliminarVehiculo = id => {
     });
 };
 
-eliminarVehiculo('MBLYfGxts7tvRfiszWtM');
+// eliminarVehiculo('MBLYfGxts7tvRfiszWtM');
+
+//Validacion del formulario
+
+const $form = document.getElementById('formulario');
+
+$form.addEventListener('submit', async e => {
+  e.preventDefault();
+
+  let nombre = e.target.nombre.value;
+  let placa = e.target.placa.value;
+  let tipovehiculo = e.target.tipo.value;
+
+  //Validacion de los campos
+  if (nombre.trim() === '') {
+    mostrarError('El nombre es obligatorio');
+    return;
+  }
+
+  if (placa.trim() === '') {
+    mostrarError('La placa es obligatoria');
+    return;
+  }
+
+  if (tipovehiculo === '') {
+    mostrarError('Seleccione el tipo de vehiculo');
+    return;
+  }
+
+  guardarVehiculo({
+    nombre,
+    placa,
+    tipovehiculo,
+    precio: tipovehiculo === 'carro' ? 1000 : 500,
+    hora: fullHora(),
+  });
+});
+
+const mostrarError = contenido => {
+  const $errorformulario = document.querySelector('.error');
+  $errorformulario.textContent = contenido;
+  $errorformulario.style.display = 'block';
+  setTimeout(() => {
+    $errorformulario.style = 'none';
+  }, 2000);
+};
+
+const mostrarGuardadoExitoso = () => {
+  const $exitoso = document.querySelector('.guardado');
+  $exitoso.style.display = 'block';
+  $form.reset();
+  setTimeout(() => {
+    $exitoso.style = 'none';
+  }, 2000);
+};
+
+const fullHora = () => {
+  let fecha = new Date();
+  return fecha.toLocaleTimeString();
+};
+
+console.log(fullHora());
